2fb84dd8c9190342a0304b2628c955c6a2ec6682
Marius Schlüter <m.schlueter01@stud.uni-goettingen.de>
2020-10-04 23:25:14 +0200

    Add project files.

---
+++ RkpCli/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RKPModels;
+
+namespace RkpCli
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if(args.Length != 3)
+            {
+                Console.Error.WriteLine("Wrong number of Arguments");
+                Console.Error.WriteLine("Usage: RkpCli <sql_host> <depmatrix_csv_file> <article_num>");
+                return 1;
+            }
+            else
+            {
+                try
+                {
+                    var sql = new MySQLArticleClient(args[0]);
+                    var mat = MaterialMatrix.ReadFromFile(args[1]);
+                    // todo get productkey
+                    string[] productkey = sql.GetProductkey(args[2]);
+                    if(productkey == null)
+                    {
+                        Console.Error.WriteLine("Article not found");
+                        return 2;
+                    }
+                    Console.WriteLine((AvailabilityCheck.IsAvailable(productkey, mat) ? 1 : 0));
+                    return 0;
+                }
+                catch (Exception exc)
+                {
+                    Console.Error.WriteLine(exc);
+                    return 3;
+                }                
+            }
+        }
+    }
+}
